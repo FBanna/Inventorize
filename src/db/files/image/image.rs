28aae58b0eec56e5d153b3cd798940964663d063
FBanna <findlaybannatyne@gmail.com>
2026-04-17 22:49:40 +0100

    1/2 done on db boiler plate

--- src/db/files/image/image.rs
+++ src/db/files/image/image.rs
@@ -12,9 +12,9 @@ use crate::{config::config::Config, error::{error::AppError, file::FileError}};
 
 #[derive(Clone, Debug, FromRow, Serialize, Deserialize)]
 pub struct ComponentImage{
-    pub component_id: i64,
-    pub full: String,
-    pub thumb: String
+    //pub component_id: i64,
+    pub full: Vec<u8>,
+    pub thumb: Vec<u8>
 }
 
 
@@ -30,26 +30,37 @@ impl ComponentImage {
 
         let img = ImageReader::new(reader).with_guessed_format()?.decode().map_err(|_| FileError::WriteUpload)?;
 
-
-        let encoder = AvifEncoder::new_with_speed_quality(w, speed, quality)
-
-        
-
         let full_img = img.resize(1000, 1000, image::imageops::FilterType::Nearest);
-
         let thumb_img = img.resize(250, 250, image::imageops::FilterType::Nearest);
 
-        let mut full_bytes = Vec::new();
-        let mut thumb_bytes = Vec::new();
+        let mut full_bytes: Vec<u8> = Vec::new();
+        let mut thumb_bytes: Vec<u8> = Vec::new();
 
-        full_img.write_to(&mut Cursor::new(&mut full_bytes), image::ImageFormat::Avif);
+        //let mut full_cursor = Cursor::new(&mut full_bytes)
 
-        thumb_img.write_to(&mut Cursor::new(&mut thumb_bytes), image::ImageFormat::Avif);
+        let full_encoder = AvifEncoder::new_with_speed_quality(Cursor::new(&mut full_bytes), 3, 25);
+        let thumb_encoder = AvifEncoder::new_with_speed_quality(Cursor::new(&mut thumb_bytes), 3, 25);
+
+
+        full_img.write_with_encoder(full_encoder);
+        thumb_img.write_with_encoder(thumb_encoder);
+
+
+
+        Ok(ComponentImage{
+            //component_id: c_id,
+            full: full_bytes,
+            thumb: thumb_bytes
+        })
+
+        // full_img.write_to(&mut Cursor::new(&mut full_bytes), image::ImageFormat::Avif);
+
+        // thumb_img.write_to(&mut Cursor::new(&mut thumb_bytes), image::ImageFormat::Avif);
 
 
 
 
-        todo!()
+        // todo!()
 
     }
 
--- src/db/files/service.rs
+++ src/db/files/service.rs
@@ -1,6 +1,7 @@
+use jsonschema::output;
 use uuid::Uuid;
 
-use crate::{db::files::file::file::ComponentFile, error::error::AppError};
+use crate::{db::{db::DB, files::{file::file::ComponentFile, image::image::ComponentImage}}, error::error::AppError};
 
 
 
@@ -8,9 +9,107 @@ use crate::{db::files::file::file::ComponentFile, error::error::AppError};
 pub trait ComponentFileService {
 
     async fn add_file(&self, c_id: i64, file: ComponentFile) -> Result<(), AppError>;
-    async fn del_file(&self, c_id: i64, uuid: String) -> Result<(), AppError>;
-    async fn get_file(&self, c_id: i64, uuid: Uuid) -> Result<ComponentFile, AppError>;
+    async fn del_file(&self, uuid: Uuid) -> Result<(), AppError>;
+    async fn get_file(&self, uuid: Uuid) -> Result<ComponentFile, AppError>;
+    async fn get_all_files(&self, c_id: i64) -> Result<Vec<ComponentFile>, AppError>;
 
-    //async fn add_image(&self, )
+    async fn add_img(&self, c_id: i64, img: ComponentImage) -> Result<(), AppError>;
+    async fn del_img(&self, c_id: i64) -> Result<(), AppError>;
+    async fn get_img(&self, c_id: i64) -> Result<ComponentImage, AppError>;
+    async fn get_full(&self, c_id: i64) -> Result<Vec<u8>, AppError>;
+    async fn get_thumb(&self, c_id: i64) -> Result<Vec<u8>, AppError>;
 
+}
+
+impl ComponentFileService for DB {
+
+
+
+    // FILES
+
+    async fn add_file(&self, c_id: i64, file: ComponentFile) -> Result<(), AppError> {
+        
+        let _output = sqlx::query("INSERT INTO component_file (file_id, component_id, name, mime) VALUES ($1, $2, $3, $4)")
+            .bind(file.file_id)
+            .bind(c_id)
+            .bind(file.name)
+            .bind(file.mime)
+            .execute(&*self.pool)
+            .await?;
+
+        Ok(())
+
+
+    }
+
+    async fn del_file(&self, uuid: Uuid) -> Result<(), AppError> {
+        
+        let _output = sqlx::query("DELETE FROM component_file WHERE file_id = ($1)")
+            .bind(uuid)
+            .execute(&*self.pool)
+            .await?;
+
+        Ok(())
+    }
+
+    async fn get_file(&self, uuid: Uuid) -> Result<ComponentFile, AppError> {
+        
+        let output: ComponentFile = sqlx::query_as("SELECT (file_id, name, mime) FROM component_file WHERE file_id = ($1)")
+            .bind(uuid)
+            .fetch_one(&*self.pool)
+            .await?;
+
+        Ok(output)
+    }
+
+    async fn get_all_files(&self, c_id: i64) -> Result<Vec<ComponentFile>, AppError> {
+        
+        let output: Vec<ComponentFile> = sqlx::query_as("SELECT (file_id, name, mime) FROM component_file WHERE component_id = ($1)")
+            .bind(c_id)
+            .fetch_all(&*self.pool)
+            .await?;
+
+        Ok(output)
+    }
+
+
+
+    // IMAGES
+
+    async fn add_img(&self, c_id: i64, img: ComponentImage) -> Result<(), AppError> {
+        
+
+        let _output = sqlx::query("INSERT INTO component_image (component_id, full, thumb) VALUES ($1, $2, $3)")
+            .bind(c_id)
+            .bind(img.full)
+            .bind(img.thumb)
+            .execute(&*self.pool)
+            .await?;
+
+        Ok(())
+    }
+
+    async fn del_img(&self, c_id: i64) -> Result<(), AppError> {
+        
+        let _output = sqlx::query("DELETE FROM component_image WHERE component_id = ($1)")
+            .bind(c_id)
+            .execute(&*self.pool)
+            .await?;
+
+        Ok(())
+    }
+
+    async fn get_img(&self, c_id: i64) -> Result<ComponentImage, AppError> {
+        
+        let output: ComponentImage = sqlx::query_as("")
+
+    }
+
+    async fn get_full(&self, c_id: i64) -> Result<Vec<u8>, AppError> {
+        todo!()
+    }
+
+    async fn get_thumb(&self, c_id: i64) -> Result<Vec<u8>, AppError> {
+        todo!()
+    }
 }
